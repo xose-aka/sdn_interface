6025e9cde4cf0a6dbd7ae39bc3bc2971866f8e97
xose-aka <xusniddin-qodirov@mail.ru>
2024-11-03 18:08:17 +0100

    message confirmation style

--- intent_client/src/features/intentChat/components/Message/index.tsx
+++ intent_client/src/features/intentChat/components/Message/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './index.scss'
 import {MessageProps} from "./index.types.ts";
+import {Col, Row} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleCheck, faRectangleXmark} from "@fortawesome/free-solid-svg-icons";
 
 function Index({ message }: MessageProps)
 {
@@ -10,8 +13,22 @@ function Index({ message }: MessageProps)
         >
             <div className="chat-message__item__timestamp">{message.timestamp.toString()}</div>
             <div className="chat-message__item">
-                <span className="chat-message__item__text">{message.message}</span>
+                <span className="chat-message__item__text">{message.text}</span>
             </div>
+            { message.sender == 'user' &&
+                <div className="chat-message__item__text__confirmation bg-light border text-center mt-1">
+                    <div className="mt-1">Do you confirm it ?</div>
+                    <hr className="m-1"/>
+                    <Row className="fs-6">
+                        <Col>
+                            <FontAwesomeIcon className="text-success cursor-pointer" icon={faCircleCheck} />
+                        </Col>
+                        <Col>
+                            <FontAwesomeIcon className="text-danger cursor-pointer" icon={faRectangleXmark} />
+                        </Col>
+                    </Row>
+                </div>
+            }
         </div>
     );
 }
